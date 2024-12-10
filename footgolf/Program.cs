784e9ba8f421f155d6766a3f164480cf48919b64
MNatan23 <163414251+MNatan23@users.noreply.github.com>
2024-12-10 12:50:01 +0100

    Add project files.

---
+++ footgolf/Program.cs
@@ -0,0 +1,25 @@
+﻿namespace footgolf
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            List<footgolf> versenyzok = new();
+            foreach (var sor in File.ReadAllLines("fob2016.txt"));
+
+            {
+                versenyzok.Add(new versenyzo(sor));
+
+            }
+
+            Console.WriteLine($"1 feladat \t versenyzők száma{versenyzok.Count()}");
+
+            int sor = 0;
+
+            if (s.kategória == "Noi")
+            {
+               sor == 1;
+            }
+        }
+    }
+}
---
+++ footgolf/footgolf.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace footgolf
+{
+    internal class footgolf
+    {
+        public string Nev { get; set; }
+        public string Kategória { get; set; }
+        public string Csapat { get; set; }
+        public string elsoeredmeny { get; set; }
+        public string masodikeredmeny { get; set; }
+        public string harmadikeredmeny { get; set; }
+        public footgolf(string sor)
+        {
+            string[] darabok = sor.Split(';');
+            Nev = darabok[0];
+            Kategória = darabok[1];
+            Csapat = darabok[2];
+            elsoeredmeny = darabok[3];
+            masodikeredmeny = darabok[4];
+            harmadikeredmeny = darabok[5];
+        }
+         
+        
+         
+         
+    }
+}
